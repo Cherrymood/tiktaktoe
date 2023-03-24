6b475b1412e801360e84c03c8accdb6967c71dcc
Valeriia <olxlerasport@gmail.com>
2023-03-24 16:05:05 -0700

    Added classes Board, Player, Application Created four states of Application

--- Program.cs
+++ Program.cs
@@ -1,11 +1,75 @@
-﻿class Application
+﻿
+class Board
 {
-    public void Start()
+    public void Print()
     {
-
+        
     }
 }
 
+class Player
+{}
+
+enum State {Preparation, Move_p1, Move_p2, Result};
+
+class Application
+{
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private State state; 
+    private string winner;
+    
+    public Application()
+    {
+        board = new Board();
+        player1 = new Player();
+        player2 = new Player();
+        state = State.Preparation;
+
+    }
+    public void Start()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case State.Preparation:
+                    Console.WriteLine("Case preparation");
+                    board.Print();
+                    state = State.Move_p1;
+                    break;
+                case State.Move_p1:
+                    Console.WriteLine("Case Move_pl1");
+                    // player1 turn
+                    // return board
+                    // analize winner
+                    // change state
+                    return;
+                case State.Move_p2:
+                    Console.WriteLine("Case Move_pl2");
+                    // player1 turn
+                    // return board
+                    // analize winner
+                    // change state
+                    break;
+                case State.Result:
+                    board.Print();
+                    Console.WriteLine("The winner is " + winner);
+                    Console.WriteLine("Want to continue playing?");
+                    string answer = Console.ReadLine();
+                    if (answer == "YES")
+                    {
+                        state = State.Preparation;
+                        continue;
+                    }
+
+                    return;
+            }
+        }
+    }
+
+}
 
 class Program
 {
@@ -13,6 +77,7 @@ class Program
     {
         Application tiktaktoe = new Application();
         tiktaktoe.Start();
+        
 
         Console.WriteLine("Application is closed");
     }
