72320296e3b248ed74086254ae0a37a5c67c9a38
Valeriia <olxlerasport@gmail.com>
2023-03-25 17:17:08 -0700

    Added Print method to Board class

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 ﻿
 class Board
 {
+    private string net = "123456789";
     public void Print()
     {
-        
+        Console.WriteLine(net.Substring(0,3));
+        Console.WriteLine(net.Substring(3,3));
+        Console.WriteLine(net.Substring(6,3));
     }
 }
 
 class Player
-{}
+{
+    public void MakesMove(ref Board board) 
+    {
+        // TO DO
+        return;
+    }
+}
+
+class BoardAnalyzer
+{
+    public static bool CheckWinner(ref Board board)
+    {
+        // TO DO
+        return true;
+    }
+}
 
 enum State {Preparation, Move_p1, Move_p2, Result};
 
@@ -26,6 +44,7 @@ class Application
         player1 = new Player();
         player2 = new Player();
         state = State.Preparation;
+        winner = " ";
 
     }
     public void Start()
@@ -41,21 +60,35 @@ class Application
                     break;
                 case State.Move_p1:
                     Console.WriteLine("Case Move_pl1");
-                    // player1 turn
-                    // return board
-                    // analize winner
-                    // change state
-                    return;
+                    player1.MakesMove(ref board);
+                    board.Print();
+                    if (BoardAnalyzer.CheckWinner(ref board))
+                    {
+                        winner = "Player1";
+                        state = State.Result;
+                    }
+                    else
+                    {
+                        state = State.Move_p2;
+                    }
+                    break;
                 case State.Move_p2:
                     Console.WriteLine("Case Move_pl2");
-                    // player1 turn
-                    // return board
-                    // analize winner
-                    // change state
+                    player2.MakesMove(ref board);
+                    board.Print();
+                    if (BoardAnalyzer.CheckWinner(ref board))
+                    {
+                        winner = "Player2";
+                        state = State.Result;
+                    }
+                    else
+                    {
+                        state = State.Move_p1;
+                    }
                     break;
                 case State.Result:
-                    board.Print();
                     Console.WriteLine("The winner is " + winner);
+                    board.Print();
                     Console.WriteLine("Want to continue playing?");
                     string answer = Console.ReadLine();
                     if (answer == "YES")
